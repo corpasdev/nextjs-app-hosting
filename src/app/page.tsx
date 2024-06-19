b119913f364671e244b25c280798ee3f420a3d6a
Brandon Corpas <corpasdev@gmail.com>
2024-06-19 15:49:30 -0500

    four commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex  bg-white min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-bold text-5xl">Hola mundo!</h1>
+    <main className="flex  bg-black min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="font-bold text-5xl">Cooweb es la mejor vamos!</h1>
 
     </main>
   );
